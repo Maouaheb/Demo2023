2b99601b065edced2f87d2854f66ddb31deea90c
Maweheb Belarbi <maouaheb.belarbi@unamur.be>
2022-10-14 12:09:21 +0200

    add somme function

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("hello infob318 xxx kfkkfdkkd");
+        System.out.println("hello somme  "+somme(4,5));
+    }
+    public static int somme (int a, int b){
+        return a+b;
     }
 }
