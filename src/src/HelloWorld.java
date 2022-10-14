6048c696267444cb20ef288573ee2b8567f1e67b
Maweheb Belarbi <maouaheb.belarbi@unamur.be>
2022-10-14 12:05:07 +0200

    modification erronée

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("hello infob318 xxx kfkkfdkkd");
+        sdmflsmdf;
+        smldsmdl,
+        fklfkdsmlf
     }
 }
