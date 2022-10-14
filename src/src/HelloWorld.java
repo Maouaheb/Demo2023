1c0ef82d656f666eaa18dd61e92e2d95c07a7024
Maouaheb <37180996+Maouaheb@users.noreply.github.com>
2022-10-14 12:16:10 +0200

    Changement de valeur a et b

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("hello somme  "+somme(4,5));
+        System.out.println("hello somme  "+somme(10,52243));
     }
     public static int somme (int a, int b){
         return a+b;
